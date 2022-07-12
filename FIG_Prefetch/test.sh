3450e25c427a7ece6cbb5de6e1ff5a72b813f9eb
deadpool <2841954631@qq.com>
2022-07-12 14:21:45 +0800

    Update FIG_Prefetch

--- FIG_Prefetch/test.sh
+++ FIG_Prefetch/test.sh
@@ -4,23 +4,16 @@
 source "../common.sh"
 ABS_PATH=$(where_is_script "$0")
 mkdir -p "$ABS_PATH"/M_DATA
-DUP_RATES=( 0 25 50 75 )
-FILE_SIZE=( $((128 * 1024)) ) # 128 * 1024
+FILE_SIZE=( $((64 * 1024)) ) # 128 * 1024
 NUM_JOBS=( 1 2 4 8 16 )
 
-# Computaion Read Parallism: 
-# 1. prefetch-current: Hint: Predict => (last) Prefetch -> Copy -> Calc -> Lookup compare  (Computaion / Read Parallism: Copy Calc Lookup || Prefetch)
-# 2. speculation: By pass "Copy Calc Lookup" and compare (last) Hint-Offset  => Eliminate Computation, Read  
-# 3. speculation+prefetch-current: (last) Prefetch -> By pass "Copy Calc Lookup" and compare (last) Hint-Offset  => Content-Comparing Read and Current Block Prefecth Read parallism.
-# 4. master: By pass "Copy Calc Lookup" and compare (last) Hint-Offset + Prefetch (cur) Hint-Offset => Content-Comparing Read and Next Block Prefecth Read parallism.
-
-FILE_SYSTEMS=( "Prefetch-Next+Speculation" "Speculation" "Prefetch-Current")
-TIMERS=( "fio_nova.sh" "fio_nova.sh" "fio_nova.sh" )
-BRANCHES=( "master" "no-prefetch" "prefetch-current" )
+FILE_SYSTEMS=( "Prefetch-Next+Speculation" "Prefetch-Next" "Speculation" "Prefetch-Current" "Naive")
+TIMERS=( "setup_nova.sh" "setup_nova.sh" "setup_nova.sh" "setup_nova.sh" "setup_nova.sh" )
+BRANCHES=( "master" "no-speculation" "no-prefetch" "prefetch-current" "no-prefetch-speculation" )
 
 
 TABLE_NAME="$ABS_PATH/performance-comparison-table"
-table_create "$TABLE_NAME" "file_system dup_rate num_job bandwidth(MiB/s)"
+table_create "$TABLE_NAME" "file_system num_job first_bandwidth(MiB/s) second_bandwidth(MiB/s) second_cmp_lat(ns) second_fp_lat(ns) second_others_lat(ns) second_lat(ns)"
 
 loop=1
 if [ "$1" ]; then
@@ -29,21 +22,35 @@ fi
 
 for ((i=1; i <= loop; i++))
 do
-    for dup_rate in "${DUP_RATES[@]}"; do
-        STEP=0
-        for file_system in "${FILE_SYSTEMS[@]}"; do
-            for fsize in "${FILE_SIZE[@]}"; do
-                for job in "${NUM_JOBS[@]}"; do
-                    EACH_SIZE=$(split_workset "$fsize" "$job")
-                    TIMER=${TIMERS[$STEP]}
+    STEP=0
+    for file_system in "${FILE_SYSTEMS[@]}"; do
+        for fsize in "${FILE_SIZE[@]}"; do
+            for job in "${NUM_JOBS[@]}"; do
+                EACH_SIZE=$(split_workset "$fsize" "$job")
+                TIMER=${TIMERS[$STEP]}
 
-                    BW=$(bash ../../nvm_tools/"$TIMER" "$job" "${EACH_SIZE}"M "$dup_rate" "${BRANCHES[$STEP]}" "0" "2M" | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ../../nvm_tools/to_MiB_s)
-                    
-                    table_add_row "$TABLE_NAME" "$file_system $dup_rate $job $BW"     
-                done
+                bash ../../nvm_tools/"${TIMER}" "${BRANCHES[$STEP]}" "1"
+                
+                BW1=$(sudo fio -directory=/mnt/pmem0 -fallocate=none -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=2M -thread -numjobs="$job" -size="${EACH_SIZE}M" -name=test --dedupe_percentage=0 -group_reporting -randseed="$i" | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ../../nvm_tools/to_MiB_s)
+
+                echo 1 > /proc/fs/NOVA/pmem0/timing_stats  
+                
+                BW2=$(sudo fio -directory=/mnt/pmem0 -fallocate=none -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=2M -thread -numjobs="$job" -size="${EACH_SIZE}M" -name=test --dedupe_percentage=0 -group_reporting -randseed="$i" | grep WRITE: | awk '{print $2}' | sed 's/bw=//g' | ../../nvm_tools/to_MiB_s)
+                
+                cat /proc/fs/NOVA/pmem0/timing_stats > "$ABS_PATH"/M_DATA/OUTPUT-"$i"
+                
+                # whole_time=$(( EACH_SIZE * 1000 * 1000 * 1000 / ("$BW2") ))  
+                whole_time=$(nova_attr_time_stats "cow_write" "$ABS_PATH"/M_DATA/OUTPUT-"$i")   
+                fp_time=$(nova_attr_time_stats "fp_calc" "$ABS_PATH"/M_DATA/OUTPUT-"$i")
+                cmp_time=$(nova_attr_time_stats "memcmp" "$ABS_PATH"/M_DATA/OUTPUT-"$i")
+                cmp_user=$(nova_attr_time_stats "cmp_user" "$ABS_PATH"/M_DATA/OUTPUT-"$i")
+                cmp_time=$((cmp_time + cmp_user)) 
+                others=$((whole_time - fp_time - cmp_time))
+                
+                table_add_row "$TABLE_NAME" "$file_system $job $BW1 $BW2 $cmp_time $fp_time $others $whole_time"     
             done
-            STEP=$((STEP + 1))
         done
+        STEP=$((STEP + 1))
     done
 done
 
